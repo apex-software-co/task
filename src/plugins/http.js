e459ebb2cf6e2b4ce43131998ce8e3d2d01fc62c
Matheus Felipe <matheus_fellip@hotmail.com>
2021-05-22 10:48:55 -0300

    dev

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -6,7 +6,7 @@ Vue.use({
   install (Vue) {
     
     let http = new Http({
-      baseURL: 'http://172.31.3.9:3000/'
+      baseURL: 'http://18.231.111.35:3000/'
     })
 
     Vue.prototype.$http = http
